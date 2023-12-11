9d396bee158141231ce73f61aa04e95f94c8fda9
Oleksandr Radkevych <aradkevich78@gmail.com>
2023-12-12 04:30:59 +0500

    Initial commit

---
+++ NET_022_Test2_Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NET_022_Test2_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
